0d728b45278b08994875c05ad337ba2380228dfc
Somanos Sar <somanos@drumee.com>
2025-06-02 09:30:22 +0200

    Changed schemas bill updated migration patch

--- schemas/patches/bill_create.sql
+++
@@ -1,2 +0,0 @@
-alter table bill add column fiscalYear integer after chrono;
-update bill set fiscalYear=fiscal_year(ctime);
--- schemas/patches/quotation_create.sql
+++
@@ -1,2 +0,0 @@
-alter table quotation add column fiscalYear integer after chrono;
-update quotation set fiscalYear=fiscal_year(ctime);
